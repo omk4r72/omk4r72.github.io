75152e17a8308c512f70ab8722984d32a40af2ba
Omkar Chavhan <94684364+omk4r72@users.noreply.github.com>
2026-01-02 16:39:56 +0530

    Update bages.js

--- bages.js
+++ bages.js
@@ -1,4 +1,15 @@
 document.getElementById("badges").innerHTML = `
-<img src="https://www.hackthebox.com/badge/image/XXXXX" />
-<img src="https://tryhackme-badges.s3.amazonaws.com/YOURNAME.png" />
+  <!-- Hack The Box Badge -->
+  <img 
+    src="https://www.hackthebox.com/badge/image/omk4r72" 
+    alt="Hack The Box Badge"
+    style="height:42px; margin:8px;"
+  />
+
+  <!-- TryHackMe Badge (IFRAME) -->
+  <iframe
+    src="https://tryhackme.com/api/v2/badges/public-profile?userPublicId=6579673"
+    style="border:none; height:60px; width:250px; margin:8px;"
+    loading="lazy">
+  </iframe>
 `;
